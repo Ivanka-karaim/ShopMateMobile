c2255935932fce094822ecb87ddc731ea54cf909
Ivanna <karaimivanna@gmail.com>
2023-12-16 12:47:02 +0200

    bottom nav view

---
+++ app/src/main/java/com/example/shopmatemobile/MainActivity2.kt
@@ -0,0 +1,40 @@
+package com.example.shopmatemobile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.shopmatemobile.databinding.ActivityMain2Binding
+
+
+class MainActivity2 : AppCompatActivity() {
+    private lateinit var binding: ActivityMain2Binding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMain2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(Home())
+
+
+        binding.bottomNavigationView.setOnItemSelectedListener{
+            when(it.itemId){
+                R.id.home -> replaceFragment(Home())
+                R.id.basket -> replaceFragment(Basket())
+                R.id.favourite -> replaceFragment(Favourite())
+                R.id.profile -> replaceFragment(Profile())
+
+                else -> {
+
+                }
+
+            }
+            true
+
+        }
+    }
+    private fun replaceFragment(fragment: Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout,fragment)
+        fragmentTransaction.commit()
+    }
+}
