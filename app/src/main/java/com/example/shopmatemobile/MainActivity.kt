9dabc555e445ad90dfd7fec61ea22bc8b2c98af7
Ivanna <karaimivanna@gmail.com>
2023-12-16 18:34:19 +0200

    Revert "transition to registration"
    This reverts commit d879b12b7c492f5fb9a8b6f6716fe63cbead4a7b.

--- app/src/main/java/com/example/shopmatemobile/MainActivity.kt
+++ app/src/main/java/com/example/shopmatemobile/MainActivity.kt
@@ -1,18 +1,13 @@
 package com.example.shopmatemobile
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
-    fun startNewActivity(view: View){
-        val intent = Intent(this, Registration::class.java)
-        startActivity(intent)
     }
 }
